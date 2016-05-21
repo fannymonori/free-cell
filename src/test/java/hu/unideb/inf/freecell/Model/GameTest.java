a3d7e4b24c2a4d0bd81df581251c941c4efdf730
fanny <fannymonori@gmail.com>
2016-05-22 00:27:10 +0200

    modifies test files

--- src/test/java/hu/unideb/inf/freecell/Model/GameTest.java
+++ src/test/java/hu/unideb/inf/freecell/Model/GameTest.java
@@ -27,70 +27,95 @@ import static org.junit.Assert.*;
  * @author fanny
  */
 public class GameTest {
-    
+
     private Game gameInstance;
     private Game gameInstance_case2;
     private Game gameInstance_case3;
-    
+    private Game gameInstance_case4;
+
     private List<List<Card>> tableau_case2;
     private List<List<Card>> tableau_case3;
-    
+    private List<List<Card>> tableau_case4;
+
     private FreeCells frecell_case2;
     private FreeCells frecell_case3;
-    
+    private FreeCells frecell_case4;
+
     private HomeCells homecell_case2;
     private HomeCells homecell_case3;
-    
+    private HomeCells homecell_case4;
+
     public GameTest() {
     }
-    
+
     @Before
     public void setUp() {
         gameInstance = new Game();
-        
+
         gameInstance_case2 = new Game();
-        
+
         gameInstance_case3 = new Game();
         
+        gameInstance_case4 = new Game();
+
         //test case 2
         tableau_case2 = new ArrayList<>();
         List<Card> pile = new ArrayList<>();
-        pile.add(new Card("hearts",10));
-        pile.add(new Card("clubs",8));
+        pile.add(new Card("hearts", 10));
+        pile.add(new Card("clubs", 8));
         tableau_case2.add(pile);
-        
+
         List<Card> pile2 = new ArrayList<>();
-        pile2.add(new Card("spades",12));
-        pile2.add(new Card("diamonds",7));
+        pile2.add(new Card("spades", 12));
+        pile2.add(new Card("diamonds", 7));
         tableau_case2.add(pile2);
-        
+
         frecell_case2 = new FreeCells();
         homecell_case2 = new HomeCells();
-        
+
         gameInstance_case2.setFreeCell(frecell_case2);
         gameInstance_case3.setHomeCell(homecell_case2);
         gameInstance_case2.setTableau(tableau_case2);
-        
+
         //test case 3
         tableau_case3 = new ArrayList<>();
         List<Card> pile_case3_1 = new ArrayList<>();
-        pile_case3_1.add(new Card("clubs",8));
-        pile_case3_1.add(new Card("hearts",1));
+        pile_case3_1.add(new Card("clubs", 8));
+        pile_case3_1.add(new Card("hearts", 1));
         tableau_case3.add(pile_case3_1);
-        
+
         List<Card> pile_case3_2 = new ArrayList<>();
-        pile_case3_2.add(new Card("spades",12));
-        pile_case3_2.add(new Card("hearts",2));
+        pile_case3_2.add(new Card("spades", 12));
+        pile_case3_2.add(new Card("hearts", 2));
         tableau_case3.add(pile_case3_2);
-        
+
         frecell_case3 = new FreeCells();
         homecell_case3 = new HomeCells();
-        
+
         gameInstance_case3.setFreeCell(frecell_case3);
         gameInstance_case3.setHomeCell(homecell_case3);
         gameInstance_case3.setTableau(tableau_case3);
+
+        //test case 4
+        tableau_case4 = new ArrayList<>();
+        List<Card> pile_case4_1 = new ArrayList<>();
+        pile_case4_1.add(new Card("clubs", 8));
+        pile_case4_1.add(new Card("hearts", 1));
+        tableau_case4.add(pile_case4_1);
+
+        List<Card> pile_case4_2 = new ArrayList<>();
+        pile_case4_2.add(new Card("spades", 12));
+        pile_case4_2.add(new Card("hearts", 2));
+        tableau_case4.add(pile_case4_2);
+
+        frecell_case4 = new FreeCells();
+        homecell_case4 = new HomeCells();
+
+        gameInstance_case4.setFreeCell(frecell_case4);
+        gameInstance_case4.setHomeCell(homecell_case4);
+        gameInstance_case4.setTableau(tableau_case4);
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -101,7 +126,7 @@ public class GameTest {
     @Test
     public void testNewGame() {
         System.out.println("newGame");
-        
+
         gameInstance.newGame();
         assertEquals(8, gameInstance.tableau.size());
     }
@@ -112,15 +137,15 @@ public class GameTest {
     @Test
     public void testAddToTableau() {
         System.out.println("addToTableau");
-        Card cardSource = new Card("diamonds",7);
-        Card cardTarget = new Card("clubs",8);
-        
+        Card cardSource = new Card("diamonds", 7);
+        Card cardTarget = new Card("clubs", 8);
+
         boolean success = gameInstance_case2.addToTableau(cardSource, cardTarget);
         assertEquals(true, success);
-        assertEquals("diamonds", gameInstance_case2.tableau.get(0).get(gameInstance_case2.tableau.get(0).size()-1).getSuit());
-        assertEquals("spades", gameInstance_case2.tableau.get(1).get(gameInstance_case2.tableau.get(1).size()-1).getSuit());
-        assertEquals(7, gameInstance_case2.tableau.get(0).get(gameInstance_case2.tableau.get(0).size()-1).getRank());
-        assertEquals(12, gameInstance_case2.tableau.get(1).get(gameInstance_case2.tableau.get(1).size()-1).getRank());
+        assertEquals("diamonds", gameInstance_case2.tableau.get(0).get(gameInstance_case2.tableau.get(0).size() - 1).getSuit());
+        assertEquals("spades", gameInstance_case2.tableau.get(1).get(gameInstance_case2.tableau.get(1).size() - 1).getSuit());
+        assertEquals(7, gameInstance_case2.tableau.get(0).get(gameInstance_case2.tableau.get(0).size() - 1).getRank());
+        assertEquals(12, gameInstance_case2.tableau.get(1).get(gameInstance_case2.tableau.get(1).size() - 1).getRank());
     }
 
     /**
@@ -129,32 +154,32 @@ public class GameTest {
     @Test
     public void testAddToHomeCell() {
         System.out.println("addToHomeCell");
-        
+
         //heart 1(ace) from tableau to homecell
-        boolean result = gameInstance_case3.addToHomeCell(gameInstance_case3.tableau.get(0).get(gameInstance_case3.tableau.get(0).size()-1),
-                gameInstance_case3.homeCells.piles.get("hearts").get(gameInstance_case3.homeCells.piles.get("hearts").size()-1));
-        assertEquals(true, result);
+        boolean success = gameInstance_case3.addToHomeCell(gameInstance_case3.tableau.get(0).get(gameInstance_case3.tableau.get(0).size() - 1),
+                gameInstance_case3.homeCells.piles.get("hearts").get(gameInstance_case3.homeCells.piles.get("hearts").size() - 1));
         
-        assertEquals("clubs", gameInstance_case3.tableau.get(0).get(gameInstance_case3.tableau.get(0).size()-1).getSuit());
-        assertEquals("hearts", gameInstance_case3.homeCells.piles.get("hearts").get(gameInstance_case3.homeCells.piles.get("hearts").size()-1).getSuit());
-        assertEquals(8, gameInstance_case3.tableau.get(0).get(gameInstance_case3.tableau.get(0).size()-1).getRank());
-        assertEquals(1, gameInstance_case3.homeCells.piles.get("hearts").get(gameInstance_case3.homeCells.piles.get("hearts").size()-1).getRank());
+        assertEquals(true, success);
+        assertEquals("clubs", gameInstance_case3.tableau.get(0).get(gameInstance_case3.tableau.get(0).size() - 1).getSuit());
+        assertEquals("hearts", gameInstance_case3.homeCells.piles.get("hearts").get(gameInstance_case3.homeCells.piles.get("hearts").size() - 1).getSuit());
+        assertEquals(8, gameInstance_case3.tableau.get(0).get(gameInstance_case3.tableau.get(0).size() - 1).getRank());
+        assertEquals(1, gameInstance_case3.homeCells.piles.get("hearts").get(gameInstance_case3.homeCells.piles.get("hearts").size() - 1).getRank());
     }
 
     /**
      * Test of addToFreeCell method, of class Game.
      */
-//    @Test
-//    public void testAddToFreeCell() {
-//        System.out.println("addToFreeCell");
-//        Card cardSource = null;
-//        Game instance = new Game();
-//        boolean expResult = false;
-//        boolean result = instance.addToFreeCell(cardSource);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+    @Test
+    public void testAddToFreeCell() {
+        System.out.println("addToFreeCell");
+        
+        boolean success = gameInstance_case4.addToFreeCell(gameInstance_case4.tableau.get(0).get(gameInstance_case4.tableau.get(0).size() - 1));
+        assertEquals(true, success);
+        assertEquals("clubs", gameInstance_case4.tableau.get(0).get(gameInstance_case4.tableau.get(0).size() - 1).getSuit());
+        assertEquals("hearts", gameInstance_case4.freeCells.cards.get(0).getSuit());
+        assertEquals(1, gameInstance_case4.freeCells.cards.get(0).getRank());
+        assertEquals(8, gameInstance_case4.tableau.get(0).get(gameInstance_case4.tableau.get(0).size() - 1).getRank());
+    }
 
     /**
      * Test of isLast method, of class Game.
@@ -170,7 +195,6 @@ public class GameTest {
 //        // TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
 //    }
-
     /**
      * Test of saveGame method, of class Game.
      */
@@ -183,7 +207,6 @@ public class GameTest {
 //        // TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
 //    }
-
     /**
      * Test of loadGame method, of class Game.
      */
@@ -196,7 +219,6 @@ public class GameTest {
 //        // TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
 //    }
-
     /**
      * Test of getSavedGames method, of class Game.
      */
@@ -210,5 +232,4 @@ public class GameTest {
 //        // TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
 //    }
-    
 }
